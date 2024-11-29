3cca5c64417a11a48da8a49648617a39c27d87e2
pgm-stijwalr <147693656+pgm-stijwalr@users.noreply.github.com>
2024-11-29 09:06:36 +0100

    login page design

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,36 +4,29 @@ import "./globals.css";
 import { SiteHeader } from "@/components/site-header";
 import { Toaster } from "react-hot-toast";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const Roboto = localFont({
+    src: "./fonts/Roboto.ttf",
+    variable: "--font-geist-sans",
+    weight: "100 900",
 });
 
 export const metadata = {
-  title: "Ecommerce App",
-  description: "A simple ecommerce application",
+    title: "Ecommerce App",
+    description: "A simple ecommerce application",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Toaster position="top-center" reverseOrder={false} />
-        <SiteHeader />
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={`${Roboto.variable} `}>
+                <Toaster position="top-center" reverseOrder={false} />
+                <SiteHeader />
+                {children}
+            </body>
+        </html>
+    );
 }
