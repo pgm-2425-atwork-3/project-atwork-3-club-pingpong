38f172cb6f7d9a8ec305c8933884d25e89dda58c
PGM-KeanoMarijsse <keano.marijsse@student.arteveldehs.be>
2024-12-20 11:03:35 +0100

    - Added an empty view component

---
+++ frontend/src/components/EmptyView.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+// import icon
+import { GiShoppingCart } from "react-icons/gi";
+
+// import the CSS
+import "@/css/empty-view.css";
+
+export default function EmptyView({ text }: { text: string }) {
+  return (
+    <div className="empty-view">
+      <p className="empty-view__text">{text}</p>
+      <GiShoppingCart className="empty-view__icon" />
+    </div>
+  );
+}
